7eb4da14c83da6c8f5c8f379ca8d14ae5183cd00
Mark Seel <markseel@Marks-MacBook.local>
2015-06-24 22:05:28 -0500

    added common header file

---
+++ lib_dsp/src/xmos_dsp_elements.h
@@ -0,0 +1,14 @@
+#ifndef XMOS_DSP_ELEMENTS
+#define XMOS_DSP_ELEMENTS
+
+#include "xmos_dsp_qformats.h"
+#include "xmos_dsp_math.h"
+#include "xmos_dsp_filters.h"
+#include "xmos_dsp_vector.h"
+#include "xmos_dsp_statistics.h"
+#include "xmos_dsp_adaptive.h"
+#include "xmos_dsp_matrix.h"
+//#include "xmos_dsp_transforms.h"
+//#include "xmos_dsp_control.h"
+
+#endif
