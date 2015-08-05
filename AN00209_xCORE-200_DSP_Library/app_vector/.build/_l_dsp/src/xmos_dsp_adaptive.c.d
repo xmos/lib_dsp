4c313d9c0be852936a1c8ed6958bb93182295a7d
John Edwards <johned0@gmail.com>
2015-08-05 11:10:58 +0100

    Test Routines Complete

--- AN00209_xCORE-200_DSP_Library/app_vector/.build/_l_dsp/src/xmos_dsp_adaptive.c.d
+++ AN00209_xCORE-200_DSP_Library/app_vector/.build/_l_dsp/src/xmos_dsp_adaptive.c.d
@@ -1,9 +1,8 @@
 .build/_l_dsp/src//xmos_dsp_adaptive.c.d ./.build/_l_dsp/src//xmos_dsp_adaptive.c.o ./.build/_l_dsp/src//xmos_dsp_adaptive.c.pca.xml: \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_adaptive.c \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_qformat.h \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_math.h \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_filters.h \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_vector.h \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_statistics.h \
-  C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_adaptive.h
-
+  ../../lib_dsp/src/xmos_dsp_adaptive.c \
+  ../../lib_dsp/src/xmos_dsp_qformat.h \
+  ../../lib_dsp/src/xmos_dsp_math.h \
+  ../../lib_dsp/src/xmos_dsp_filters.h \
+  ../../lib_dsp/src/xmos_dsp_vector.h \
+  ../../lib_dsp/src/xmos_dsp_statistics.h \
+  ../../lib_dsp/src/xmos_dsp_adaptive.h
