30f72b70ebe4831f0599224d494fe7ff4ae40e76
John Edwards <john@xmos.com>
2015-08-12 23:13:26 +0100

    Tests updated to support NMLS

--- AN00209_xCORE-200_DSP_Library/app_adaptive/.build/xmos_dsp_adaptive.d
+++ AN00209_xCORE-200_DSP_Library/app_adaptive/.build/xmos_dsp_adaptive.d
@@ -1,4 +1,4 @@
-.build/_l_dsp/src//xmos_dsp_adaptive.c.d ./.build/_l_dsp/src//xmos_dsp_adaptive.c.o ./.build/_l_dsp/src//xmos_dsp_adaptive.c.pca.xml: \
+xmos_dsp_adaptive.o: \
   C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_adaptive.c \
   C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_qformat.h \
   C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_math.h \
@@ -6,4 +6,3 @@
   C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_vector.h \
   C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_statistics.h \
   C:/Users/johne2/.myrepos/lib_dsp/lib_dsp/src/xmos_dsp_adaptive.h
-
