dab37af487754e03175630179420e11925008b5a
Matt Fyles <matt@xmos.com>
2015-08-13 18:01:22 +0100

    Add top level header to include all other headers

---
+++ api/lib_dsp.h
@@ -0,0 +1,12 @@
+fndef __dsp_h__
+#define __dsp_h__
+#include <../lib_dsp/xmos_dsp_adaptive.h>
+#include <../lib_dsp/xmos_dsp_filters.h>
+#include <../lib_dsp/xmos_dsp_matrix.h>
+#include <../lib_dsp/xmos_dsp_statistics.h>
+#include <../lib_dsp/xmos_dsp_elements.h>
+#include <../lib_dsp/xmos_dsp_math.h>
+#include <../lib_dsp/xmos_dsp_qformat.h>
+#include <../lib_dsp/xmos_dsp_vector.h>
+#endif
+
