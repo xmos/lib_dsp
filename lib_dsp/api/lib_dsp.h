e88283c5228556fee582dfe95e7638087fc244db
Mark Seel <markseel@Marks-MacBook.local>
2015-09-09 17:23:51 -0500

    Changed include filenames

--- lib_dsp/api/lib_dsp.h
+++ lib_dsp/api/lib_dsp.h
@@ -1,13 +1,16 @@
 // Copyright (c) 2015, XMOS Ltd, All rights reserved
+
 #ifndef __dsp_h__
 #define __dsp_h__
-#include <../lib_dsp/xmos_dsp_adaptive.h>
-#include <../lib_dsp/xmos_dsp_filters.h>
-#include <../lib_dsp/xmos_dsp_matrix.h>
-#include <../lib_dsp/xmos_dsp_statistics.h>
-#include <../lib_dsp/xmos_dsp_elements.h>
-#include <../lib_dsp/xmos_dsp_math.h>
-#include <../lib_dsp/xmos_dsp_qformat.h>
-#include <../lib_dsp/xmos_dsp_vector.h>
+
+#include <../lib_dsp/lib_dsp_adaptive.h>
+#include <../lib_dsp/lib_dsp_filters.h>
+#include <../lib_dsp/lib_dsp_matrix.h>
+#include <../lib_dsp/lib_dsp_statistics.h>
+#include <../lib_dsp/lib_dsp_elements.h>
+#include <../lib_dsp/lib_dsp_math.h>
+#include <../lib_dsp/lib_dsp_qformat.h>
+#include <../lib_dsp/lib_dsp_vector.h>
+
 #endif
 
