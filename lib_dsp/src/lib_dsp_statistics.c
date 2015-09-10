3fa372d112f3742a11342e4a1acf8cbde36b8b08
Mark Seel <markseel@Marks-MacBook.local>
2015-09-09 20:50:06 -0500

    corrections to comments

--- lib_dsp/src/lib_dsp_statistics.c
+++ lib_dsp/src/lib_dsp_statistics.c
@@ -1,18 +1,9 @@
-// ============================================================================
 // Copyright (c) 2015, XMOS Ltd, All rights reserved
 
 #include <platform.h>
 #include "lib_dsp_qformat.h"
 #include "lib_dsp_math.h"
 #include "lib_dsp_statistics.h"
-
-// ============================================================================
-
-// Vector sum: result = X[0] + ... X[N-1]
-//
-// 'input_vector_X': Pointer to source data array X.
-// 'vector_length':  Length of the input vector.
-// 'q_format':       Fixed point format, the number of bits making up fractional part.
 
 int lib_dsp_vector_abs_sum
 (
@@ -62,13 +53,28 @@ int lib_dsp_vector_abs_sum
     return ah;
 }
 
-// ============================================================================
-
-// Vector mean: result = (X[0] + ... X[N-1]) / N
-//
-// 'input_vector_X': Pointer to source data array X.
-// 'vector_length':  Length of the input vector.
-// 'q_format':       Fixed point format, the number of bits making up fractional part.
+/** Vector mean: ``R = (X[0] + X[N-1]) / N``
+ * 
+ *  This function computes the mean of the values contained within
+ *  the input vector.
+ * 
+ *  Due to successive 32-bit additions being accumulated using 64-bit
+ *  arithmetic overflow during the summation process is unlikely. The final
+ *  value, being effectively the result of a left-shift by ``q_format`` bits
+ *  will potentially overflow the final fixed-point value depending on the
+ *  resulting summed value and the chosen Q-format.
+ * 
+ *  Example:
+ * 
+ *  \code
+ *  int result;
+ *  result = lib_dsp_vector_mean( input_vector, 256, 28 );
+ *  \endcode
+ * 
+ *  \param  input_vector_X  Pointer to source data array X.
+ *  \param  vector_length   Length of the input vector.
+ *  \param  q_format        Fixed point format (i.e. number of fractional bits).
+ */
 
 int lib_dsp_vector_mean
 (
@@ -81,13 +87,30 @@ int lib_dsp_vector_mean
     return lib_dsp_math_multiply( divide_by_N, vectort_sum, q_format );
 }
 
-// ============================================================================
-
-// Vector power (sum of squares): result = X[0]^2 + ... X[N-1]^2
-//
-// 'input_vector_X': Pointer to source data array X.
-// 'vector_length':  Length of the input vector.
-// 'q_format':       Fixed point format, the number of bits making up fractional part.
+/** Vector power (sum of squares): ``R = X[0]^2 + X[N-1]^2``
+ * 
+ *  This function computes the power (also know as the sum-of-squares) of the
+ *  values contained within the input vector.
+ * 
+ *  Since each element in the vector is squared the behavior for fixed-point
+ *  multiplication should be considered (see behavior for the function
+ *  ``lib_dsp_math_multiply``). Due to successive 32-bit additions being
+ *  accumulated using 64-bit arithmetic overflow during the summation process
+ *  is unlikely. The final value, being effectively the result of a left-shift
+ *  by ``q_format`` bits will potentially overflow the final fixed-point value
+ *  depending on the resulting summed value and the chosen Q-format.
+ * 
+ *  Example:
+ * 
+ *  \code
+ *  int result;
+ *  result = lib_dsp_vector_power( input_vector, 256, 28 );
+ *  \endcode
+ * 
+ *  \param  input_vector_X  Pointer to source data array X.
+ *  \param  vector_length   Length of the input vector.
+ *  \param  q_format        Fixed point format (i.e. number of fractional bits).
+ */
 
 int lib_dsp_vector_power
 (
@@ -135,13 +158,38 @@ int lib_dsp_vector_power
     return ah;
 }
 
-// ============================================================================
-
-// Vector root mean square: result = ((X[0]^2 + ... X[N-1]^2) / N) ^ 0.5)
-//
-// 'input_vector_X': Pointer to source data array X.
-// 'vector_length':  Length (N) of the input vector.
-// 'q_format':       Fixed point format, the number of bits making up fractional part.
+/** Vector root mean square: ``R = ((X[0]^2 + X[N-1]^2) / N) ^ 0.5)``
+ * 
+ *  This function computes the root-mean-square (RMS) of the values contained
+ *  within the input vector.
+ * 
+ *  \code
+ *  result = 0
+ *  for i = 0 to N-1: result += input_vector_X[i]
+ *  return lib_dsp_math_squareroot( result / vector_length )
+ *  \endcode
+ * 
+ *  Since each element in the vector is squared the behavior for fixed-point
+ *  multiplication should be considered (see behavior for the function
+ *  ``lib_dsp_math_multiply``). Due to successive 32-bit additions being
+ *  accumulated using 64-bit arithmetic overflow during the summation
+ *  process is unlikely. The squareroot of the 'sum-of-squares divided by N
+ *  uses the function ``lib_dsp_math_squareroot``; see behavior for that
+ *  function. The final value, being effectively the result of a left-shift by
+ *  ``q_format`` bits will potentially overflow the final fixed-point value
+ *  depending on the resulting summed value and the chosen Q-format.
+ * 
+ *  Example:
+ * 
+ *  \code
+ *  int result;
+ *  result = lib_dsp_vector_rms( input_vector, 256, 28 );
+ *  \endcode
+ * 
+ *  \param  input_vector_X  Pointer to source data array X.
+ *  \param  vector_length   Length (N) of the input vector.
+ *  \param  q_format        Fixed point format (i.e. number of fractional bits).
+ */
 
 int lib_dsp_vector_rms
 (
@@ -156,14 +204,31 @@ int lib_dsp_vector_rms
     return rt_mean_sqr;
 }
 
-// ============================================================================
-
-// Vector dot product: return = X[0] * Y[0] + ... X[N-1] * Y[N-1]
-//
-// 'input_vector_X': Pointer to source data array X.
-// 'input_vector_Y': Pointer to source data array Y.
-// 'vector_length':  Length of the input vectors.
-// 'q_format':       Fixed point format, the number of bits making up fractional part.
+/** Vector dot product: ``R = X[0] * Y[0] + X[N-1] * Y[N-1]``
+ * 
+ *  This function computes the dot-product of two equal length vectors.
+ * 
+ *  The elements in the input vectors are multiplied before being summed
+ *  therefore fixed-point multiplication behavior must be considered
+ *  (see behavior for the function ``lib_dsp_math_multiply``). Due to
+ *  successive 32-bit additions being accumulated using 64-bit arithmetic
+ *  overflow during the summation process is unlikely. The final value, being
+ *  effectively the result of a left-shift by ``q_format`` bits will
+ *  potentially overflow the final fixed-point value depending on the resulting
+ *  summed value and the chosen Q-format.
+ * 
+ *  Example:
+ * 
+ *  \code
+ *  int result;
+ *  result = lib_dsp_vector_dotprod( input_vector, 256, 28 );
+ *  \endcode
+ * 
+ *  \param  input_vector_X  Pointer to source data array X.
+ *  \param  input_vector_Y  Pointer to source data array Y.
+ *  \param  vector_length   Length of the input vectors.
+ *  \param  q_format        Fixed point format (i.e. number of fractional bits).
+ */
 
 int lib_dsp_vector_dotprod
 (
@@ -213,5 +278,3 @@ int lib_dsp_vector_dotprod
     asm("lextract %0,%1,%2,%3,32":"=r"(ah):"r"(ah),"r"(al),"r"(q_format));
     return ah;
 }
-
-// ============================================================================
