5e5c4aa0ae16f25a69a6bc22d32f6d51c2bc2ccb
Mark Seel <markseel@Marks-MacBook.local>
2015-11-20 09:21:55 -0600

    Fixed illegal character issue

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -3,9 +3,9 @@ import xmostest
 def runtest():
     resources = xmostest.request_resource("xsim")
      
-    tester = xmostest.ComparisonTester(open(‘transforms_test.expect'),
+    tester = xmostest.ComparisonTester(open('transforms_test.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_transforms’, {})
+                                       'app_transforms', {})
      
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/app_transforms.xe',
