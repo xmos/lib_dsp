7dfa81ec11f835235d7867a537e29e176cd279e5
ThomasGmeinder <thomas@xmos.com>
2015-12-21 10:31:12 +0100

    Added test for complex forward FFT of two short int signals. Added test for complex forward 32 bit FFT and  inverse FFT.

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -4,10 +4,34 @@ import xmostest
 def runtest():
     resources = xmostest.request_resource("xsim")
      
-    tester = xmostest.ComparisonTester(open('transforms_test.expect'),
+    tester = xmostest.ComparisonTester(open('tworeal_fft_test.expect'),
                                        'lib_dsp', 'simple_tests',
                                        'app_transforms', {})
      
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/tworeal_fft/app_transforms_tworeal_fft.xe',
                               tester=tester)
+
+
+    resources = xmostest.request_resource("xsim")
+     
+    tester = xmostest.ComparisonTester(open('complex_fft_test.expect'),
+                                       'lib_dsp', 'simple_tests',
+                                       'app_transforms', {})
+     
+    xmostest.run_on_simulator(resources['xsim'],
+                              '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/complex_fft/app_transforms_complex_fft.xe',
+                              tester=tester)
+
+
+    resources = xmostest.request_resource("xsim")
+     
+    tester = xmostest.ComparisonTester(open('complex_short_int_fft_test.expect'),
+                                       'lib_dsp', 'simple_tests',
+                                       'app_fft_short_int', {})
+     
+    xmostest.run_on_simulator(resources['xsim'],
+                              '../AN00209_xCORE-200_DSP_Library/app_fft_short_int/bin/app_fft_short_int.xe',
+                              tester=tester)
+
+
