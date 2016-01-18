bc1961ee8ba8c94ba17820e233f26ad47d4cd4c2
ThomasGmeinder <thomas@xmos.com>
2016-01-18 15:44:15 +0100

    Fixed bugs in conversion macros. They used a different divider for positive and negative numbers which caused different spacing of positive and negative numbers. Added macro with parameter n to parameterise the Qn and Fn conversion macros

--- lib_dsp/api/lib_dsp_qformat.h
+++ lib_dsp/api/lib_dsp_qformat.h
@@ -3,44 +3,55 @@
 #ifndef LIB_DSP_QFORMAT
 #define LIB_DSP_QFORMAT
 
-// Compute fixed-point value from floating-point value
+#include <math.h>
 
-#define Q31(f) (((f)<0)?((int)(2147483648.0*(f)-0.5)):((int)(2147483647.0*(f)+0.5)))
-#define Q30(f) (((f)<0)?((int)(1073741824.0*(f)-0.5)):((int)(1073741823.0*(f)+0.5)))
-#define Q29(f) (((f)<0)?((int)( 536870912.0*(f)-0.5)):((int)( 536870911.0*(f)+0.5)))
-#define Q28(f) (((f)<0)?((int)( 268435456.0*(f)-0.5)):((int)( 268435455.0*(f)+0.5)))
-#define Q27(f) (((f)<0)?((int)( 134217728.0*(f)-0.5)):((int)( 134217727.0*(f)+0.5)))
-#define Q26(f) (((f)<0)?((int)(  67108864.0*(f)-0.5)):((int)(  67108863.0*(f)+0.5)))
-#define Q25(f) (((f)<0)?((int)(  33554432.0*(f)-0.5)):((int)(  33554431.0*(f)+0.5)))
-#define Q24(f) (((f)<0)?((int)(  16777216.0*(f)-0.5)):((int)(  16777215.0*(f)+0.5)))
-#define Q23(f) (((f)<0)?((int)(   8388608.0*(f)-0.5)):((int)(   8388607.0*(f)+0.5)))
-#define Q22(f) (((f)<0)?((int)(   4194304.0*(f)-0.5)):((int)(   4194303.0*(f)+0.5)))
-#define Q21(f) (((f)<0)?((int)(   2097152.0*(f)-0.5)):((int)(   2097151.0*(f)+0.5)))
-#define Q20(f) (((f)<0)?((int)(   1045876.0*(f)-0.5)):((int)(   1045875.0*(f)+0.5)))
-#define Q19(f) (((f)<0)?((int)(    524288.0*(f)-0.5)):((int)(    524287.0*(f)+0.5)))
-#define Q18(f) (((f)<0)?((int)(    262144.0*(f)-0.5)):((int)(    262143.0*(f)+0.5)))
-#define Q17(f) (((f)<0)?((int)(    131072.0*(f)-0.5)):((int)(    131071.0*(f)+0.5)))
-#define Q16(f) (((f)<0)?((int)(     65536.0*(f)-0.5)):((int)(     65535.0*(f)+0.5)))
+/** These Macros can be used to parameterize the conversion macros.
+ * E.g.
+ * #define BP 20  // location of the binary point
+ * Then use the macro like this: F(BP)(fixed_point_val)
+ */
+#define F0(N) F ## N
+#define F(N) F0(N)
 
-// Compute floating-point value from fixed-point value
+#define Q0(N) Q ## N
+#define Q(N) Q0(N)
 
-#define F31(x) (((int)(x)<0)?((float)(int)(x))/2147483648.0:((float)(x))/2147483647.0)
-#define F30(x) (((int)(x)<0)?((float)(int)(x))/1073741824.0:((float)(x))/1073741823.0)
-#define F29(x) (((int)(x)<0)?((float)(int)(x))/ 536870912.0:((float)(x))/ 536870911.0)
-#define F28(x) (((int)(x)<0)?((float)(int)(x))/ 268435456.0:((float)(x))/ 268435455.0)
-#define F27(x) (((int)(x)<0)?((float)(int)(x))/ 134217728.0:((float)(x))/ 134217727.0)
-#define F26(x) (((int)(x)<0)?((float)(int)(x))/  67108864.0:((float)(x))/  67108863.0)
-#define F25(x) (((int)(x)<0)?((float)(int)(x))/  33554432.0:((float)(x))/  33554431.0)
-#define F24(x) (((int)(x)<0)?((float)(int)(x))/  16777216.0:((float)(x))/  16777215.0)
-#define F23(x) (((int)(x)<0)?((float)(int)(x))/   8388608.0:((float)(x))/   8388607.0)
-#define F22(x) (((int)(x)<0)?((float)(int)(x))/   4194304.0:((float)(x))/   4194303.0)
-#define F21(x) (((int)(x)<0)?((float)(int)(x))/   2097152.0:((float)(x))/   2097151.0)
-#define F20(x) (((int)(x)<0)?((float)(int)(x))/   1045876.0:((float)(x))/   1045875.0)
-#define F19(x) (((int)(x)<0)?((float)(int)(x))/    524288.0:((float)(x))/    524287.0)
-#define F18(x) (((int)(x)<0)?((float)(int)(x))/    262144.0:((float)(x))/    262143.0)
-#define F17(x) (((int)(x)<0)?((float)(int)(x))/    131072.0:((float)(x))/    131071.0)
-#define F16(x) (((int)(x)<0)?((float)(int)(x))/     65536.0:((float)(x))/     65535.0)
-#define F15(x) (((int)(x)<0)?((float)(int)(x))/     32768.0:((float)(x))/     32767.0)
+// Convert from fixed point to double precision floating point
+#define Q31(f) round(f * (double)(1<<31))
+#define Q30(f) round(f * (double)(1<<30))
+#define Q29(f) round(f * (double)(1<<29))
+#define Q28(f) round(f * (double)(1<<28))
+#define Q27(f) round(f * (double)(1<<27))
+#define Q26(f) round(f * (double)(1<<26))
+#define Q25(f) round(f * (double)(1<<25))
+#define Q24(f) round(f * (double)(1<<24))
+#define Q23(f) round(f * (double)(1<<23))
+#define Q22(f) round(f * (double)(1<<22))
+#define Q21(f) round(f * (double)(1<<21))
+#define Q20(f) round(f * (double)(1<<20))
+#define Q19(f) round(f * (double)(1<<19))
+#define Q18(f) round(f * (double)(1<<18))
+#define Q17(f) round(f * (double)(1<<17))
+#define Q16(f) round(f * (double)(1<<16))
+
+// Convert from double precision floating point to fixed point
+#define F31(x) ((double)(x)/(double)(1<<31))
+#define F30(x) ((double)(x)/(double)(1<<30))
+#define F29(x) ((double)(x)/(double)(1<<29))
+#define F28(x) ((double)(x)/(double)(1<<28))
+#define F27(x) ((double)(x)/(double)(1<<27))
+#define F26(x) ((double)(x)/(double)(1<<26))
+#define F25(x) ((double)(x)/(double)(1<<25))
+#define F24(x) ((double)(x)/(double)(1<<24))
+#define F23(x) ((double)(x)/(double)(1<<23))
+#define F22(x) ((double)(x)/(double)(1<<22))
+#define F21(x) ((double)(x)/(double)(1<<21))
+#define F20(x) ((double)(x)/(double)(1<<20))
+#define F19(x) ((double)(x)/(double)(1<<19))
+#define F18(x) ((double)(x)/(double)(1<<18))
+#define F17(x) ((double)(x)/(double)(1<<17))
+#define F16(x) ((double)(x)/(double)(1<<16))
+#define F15(x) ((double)(x)/(double)(1<<15))
 
 #endif
 
