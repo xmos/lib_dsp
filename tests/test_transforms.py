9c5bb8845b2d800565d67df8a73fc4a5f23a40e3
Sam Chesney <samc@xmos.com>
2016-01-25 17:40:37 +0000

    Add test configuration strings
    Adding unique configuration name strings to each test to avoid results
    being overwritten.

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -5,7 +5,7 @@ def runtest():
 
     tester = xmostest.ComparisonTester(open('tworeal_fft_test.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_transforms', {})
+                                       'app_transforms', {'config':'tworeals'})
 
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/tworeals_fft/app_transforms_tworeals_fft.xe',
@@ -16,7 +16,7 @@ def runtest():
 
     tester = xmostest.ComparisonTester(open('complex_fft_test.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_transforms', {})
+                                       'app_transforms', {'config':'complex'})
 
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/complex_fft/app_transforms_complex_fft.xe',
@@ -27,7 +27,7 @@ def runtest():
 
     tester = xmostest.ComparisonTester(open('app_fft_short_int_two_complex.xe.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_fft_short_int', {})
+                                       'app_fft_short_int', {'config':'two_complex'})
 
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_fft_short_int/bin/two_complex/app_fft_short_int_two_complex.xe',
@@ -37,7 +37,7 @@ def runtest():
 
     tester = xmostest.ComparisonTester(open('app_fft_short_int_complex.xe.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_fft_short_int', {})
+                                       'app_fft_short_int', {'config':'complex'})
 
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_fft_short_int/bin/complex/app_fft_short_int_complex.xe',
@@ -45,7 +45,7 @@ def runtest():
 
     tester = xmostest.ComparisonTester(open('app_fft_short_int_tworeals.xe.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_fft_short_int', {})
+                                       'app_fft_short_int', {'config':'tworeals'})
 
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_fft_short_int/bin/tworeals/app_fft_short_int_tworeals.xe',
