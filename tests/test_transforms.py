966d79064eda7b7920bd488ccc232809e7e4b86e
Sam Chesney <samc@xmos.com>
2016-01-25 17:45:22 +0000

    Improve readability of test script

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,6 +1,7 @@
 import xmostest
 
 def runtest():
+    # Test app_transforms: 'tworeals' configuration
     resources = xmostest.request_resource("xsim")
 
     tester = xmostest.ComparisonTester(open('tworeal_fft_test.expect'),
@@ -11,7 +12,7 @@ def runtest():
                               '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/tworeals_fft/app_transforms_tworeals_fft.xe',
                               tester=tester)
 
-
+    # Test app_transforms: 'complex' configuration
     resources = xmostest.request_resource("xsim")
 
     tester = xmostest.ComparisonTester(open('complex_fft_test.expect'),
@@ -22,7 +23,7 @@ def runtest():
                               '../AN00209_xCORE-200_DSP_Library/app_transforms/bin/complex_fft/app_transforms_complex_fft.xe',
                               tester=tester)
 
-
+    # Test app_fft_short_int: 'two_complex' configuration
     resources = xmostest.request_resource("xsim")
 
     tester = xmostest.ComparisonTester(open('app_fft_short_int_two_complex.xe.expect'),
@@ -33,6 +34,7 @@ def runtest():
                               '../AN00209_xCORE-200_DSP_Library/app_fft_short_int/bin/two_complex/app_fft_short_int_two_complex.xe',
                               tester=tester)
 
+    # Test app_fft_short_int: 'complex' configuration
     resources = xmostest.request_resource("xsim")
 
     tester = xmostest.ComparisonTester(open('app_fft_short_int_complex.xe.expect'),
@@ -42,6 +44,9 @@ def runtest():
     xmostest.run_on_simulator(resources['xsim'],
                               '../AN00209_xCORE-200_DSP_Library/app_fft_short_int/bin/complex/app_fft_short_int_complex.xe',
                               tester=tester)
+
+    # Test app_fft_short_int: 'tworeals' configuration
+    resources = xmostest.request_resource("xsim")
 
     tester = xmostest.ComparisonTester(open('app_fft_short_int_tworeals.xe.expect'),
                                        'lib_dsp', 'simple_tests',
