f990cc04779ad74443ed4ae1824dfcabcfea2af9
Sam Chesney <samc@xmos.com>
2016-08-03 11:44:28 +0100

    Fix indentation

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -20,7 +20,7 @@ class DeletyComparisonTester(xmostest.ComparisonTester):
         super(DeletyComparisonTester, self).run(output)
         shutil.rmtree(self.d)
 
-       
+
 def do_fft_test(length_log2, testlevel, test_dir_name, test_name):
 
     seed  = random.randrange(4294967294);
@@ -34,7 +34,7 @@ def do_fft_test(length_log2, testlevel, test_dir_name, test_name):
     gen_test.generate(180, length_log2, 1, seed, source_directory)
     shutil.copy(os.path.join(test_dir_name,'src','test.xc'), source_directory)
     shutil.copy(os.path.join(test_dir_name,'Makefile'), directory_name)
-    
+
     resources = xmostest.request_resource("xsim")
 
     binary = os.path.join(directory_name, 'bin', 'test.xe')
@@ -44,7 +44,7 @@ def do_fft_test(length_log2, testlevel, test_dir_name, test_name):
                                        'fft_tests',
                                        test_name, directory_name,
                                        {'FFT length':(1<<length_log2), 'Seed':seed})
-                                       
+
     tester.set_min_testlevel(testlevel)
 
     xmostest.run_on_simulator(resources['xsim'], binary,
@@ -52,17 +52,15 @@ def do_fft_test(length_log2, testlevel, test_dir_name, test_name):
                               tester = tester)
 
 def runtest():
-  try:
-   for r in range(3, 14):
-       do_fft_test(r, "smoke", 'test_fft_forward', "forward_fft")
-       do_fft_test(r, "smoke", 'test_fft_inverse', "inverse_fft")
-       do_fft_test(r, "smoke", 'test_fft_index_bit_reverse', "index_bit_reversal")
-       do_fft_test(r, "smoke", 'test_fft_split_and_merge', "fft_split_and_merge")
-       do_fft_test(r, "smoke", 'test_fft_short_long', "short_and_long_conversion ")
-  except:
-    #clean everything up
-    for file in os.listdir("."):
-      if file.startswith("temp_fft_dir_"):
-        shutil.rmtree(file)
-        
-        
+    try:
+        for r in range(3, 14):
+            do_fft_test(r, "smoke", 'test_fft_forward', "forward_fft")
+            do_fft_test(r, "smoke", 'test_fft_inverse', "inverse_fft")
+            do_fft_test(r, "smoke", 'test_fft_index_bit_reverse', "index_bit_reversal")
+            do_fft_test(r, "smoke", 'test_fft_split_and_merge', "fft_split_and_merge")
+            do_fft_test(r, "smoke", 'test_fft_short_long', "short_and_long_conversion ")
+    except:
+        #clean everything up
+        for file in os.listdir("."):
+            if file.startswith("temp_fft_dir_"):
+                shutil.rmtree(file)
