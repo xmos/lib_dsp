62f6cefca172659f9e1e2a594c084c6a40c213f8
Peter Hedinger <peter@xmos.com>
2016-08-23 08:21:34 +0100

    Use extern "C" to simplify the headers.

--- lib_dsp/api/dsp_ds3.h
+++ lib_dsp/api/dsp_ds3.h
@@ -3,6 +3,10 @@
 #ifndef _FIRDS3_H_
 #define _FIRDS3_H_
 
+#ifdef __XC__
+extern "C" {
+#endif
+
     // ===========================================================================
     //
     // Defines
@@ -38,23 +42,6 @@
 
         // FIRDS3 Ctrl
         // -----------
-#ifdef __XC__
-        typedef struct _FIRDS3Ctrl                                      
-        {
-            int*unsafe                                  piIn;           // Pointer to input data (3 samples)
-            int*unsafe                                      piOut;          // Pointer to output data (1 sample)
-
-            int*unsafe                                      piDelayB;       // Pointer to delay line base
-            unsigned int                            uiDelayL;       // Total length of delay line
-            int*unsafe                                      piDelayI;       // Pointer to current position in delay line
-            int*unsafe                                      piDelayW;       // Delay buffer wrap around address (for circular buffer simulation)
-            unsigned int                            uiDelayO;       // Delay line offset for second write (for circular buffer simulation)
-
-            unsigned int                            uiNLoops;       // Number of inner loop iterations
-            unsigned int                            uiNCoefs;       // Number of coefficients
-            int*unsafe                                      piCoefs;        // Pointer to coefficients
-        } FIRDS3Ctrl_t;
-#else
         typedef struct _FIRDS3Ctrl
         {
             int*                                    piIn;           // Pointer to input data (3 samples)
@@ -70,7 +57,6 @@
             unsigned int                            uiNCoefs;       // Number of coefficients
             int*                                    piCoefs;        // Pointer to coefficients
         } FIRDS3Ctrl_t;
-#endif
 
 /** This function initialises the decimate by 3 function for a given instance
  *
@@ -78,22 +64,14 @@
  *  \param      *psFIRDS3Ctrl   DS3 control structure of type FIRDS3Ctrl_t
  *  \returns    Error code of type FIRDS3ReturnCodes_t
  */
-#ifdef __XC__
-        FIRDS3ReturnCodes_t             FIRDS3_init(FIRDS3Ctrl_t* unsafe psFIRDS3Ctrl);
-#else
         FIRDS3ReturnCodes_t             FIRDS3_init(FIRDS3Ctrl_t* psFIRDS3Ctrl);
-#endif
 /** This function clears the decimate by 3 delay line for a given instance
  *
  *
  *  \param      *psFIRDS3Ctrl   DS3 control structure of type FIRDS3Ctrl_t
  *  \returns    Error code of type FIRDS3ReturnCodes_t
  */
-#ifdef __XC__
-        FIRDS3ReturnCodes_t             FIRDS3_sync(FIRDS3Ctrl_t* unsafe psFIRDS3Ctrl);
-#else
         FIRDS3ReturnCodes_t             FIRDS3_sync(FIRDS3Ctrl_t* psFIRDS3Ctrl);
-#endif
 /** This function performs the decimation on three input samples and outputs on sample
  *  The input and output buffers are pointed to by members of the psFIRDS3Ctrl structure
  *
@@ -101,11 +79,11 @@
  *  \param      *psFIRDS3Ctrl   DS3 control structure of type FIRDS3Ctrl_t
  *  \returns    Error code of type FIRDS3ReturnCodes_t
  */
-#ifdef __XC__
-        FIRDS3ReturnCodes_t             FIRDS3_proc(FIRDS3Ctrl_t* unsafe psFIRDS3Ctrl);
-#else
         FIRDS3ReturnCodes_t             FIRDS3_proc(FIRDS3Ctrl_t* psFIRDS3Ctrl);
-#endif
     #endif // nINCLUDE_FROM_ASM
+
+#ifdef __XC__
+}
+#endif
 
 #endif // _FIRDS3_H_
--- lib_dsp/api/dsp_os3.h
+++ lib_dsp/api/dsp_os3.h
@@ -4,6 +4,10 @@
 #ifndef _FIROS3_H_
 #define _FIROS3_H_
 
+#ifdef __XC__
+extern "C" {
+#endif
+
     // ===========================================================================
     //
     // Defines
@@ -45,24 +49,6 @@
 
         // FIROS3 Ctrl
         // -----------
-#ifdef __XC__
-        typedef struct _FIROS3Ctrl                                      
-        {
-            int                                     iIn;            // Input data (to be updated every 3 output samples, i.e. when iPhase == 0)
-            int                                     iOut;           // Output data (1 sample)
-            int                                     iPhase;         // Current output phase (when reaching '0', a new input sample is required)
-
-            int* unsafe                             piDelayB;       // Pointer to delay line base
-            unsigned int                            uiDelayL;       // Total length of delay line
-            int* unsafe                             piDelayI;       // Pointer to current position in delay line
-            int* unsafe                             piDelayW;       // Delay buffer wrap around address (for circular buffer simulation)
-            unsigned int                            uiDelayO;       // Delay line offset for second write (for circular buffer simulation)
-
-            unsigned int                            uiNLoops;       // Number of inner loop iterations
-            unsigned int                            uiNCoefs;       // Number of coefficients
-            int* unsafe                             piCoefs;        // Pointer to coefficients
-        } FIROS3Ctrl_t;
-#else
         typedef struct _FIROS3Ctrl                                      
         {
             int                                     iIn;            // Input data (to be updated every 3 output samples, i.e. when iPhase == 0)
@@ -79,7 +65,6 @@
             unsigned int                            uiNCoefs;       // Number of coefficients
             int*                                    piCoefs;        // Pointer to coefficients
         } FIROS3Ctrl_t;
-#endif
 
         
 /** This function initialises the oversample by 3 function for a given instance
@@ -88,11 +73,7 @@
  *  \param      *psFIROS3Ctrl   OS3 control structure of type FIROS3Ctrl_t
  *  \returns    Error code of type FIRDS3ReturnCodes_t
  */
-#ifdef __XC__        
-        FIROS3ReturnCodes_t             FIROS3_init(FIROS3Ctrl_t* unsafe psFIROS3Ctrl);
-#else
         FIROS3ReturnCodes_t             FIROS3_init(FIROS3Ctrl_t* psFIROS3Ctrl);
-#endif  
 
 /** This function clears the oversample by 3 delay line for a given instance
  *
@@ -100,11 +81,7 @@
  *  \param      *psFIROS3Ctrl   OS3 control structure of type FIROS3Ctrl_t
  *  \returns    Error code of type FIRDS3ReturnCodes_t
  */
-#ifdef __XC__
-        FIROS3ReturnCodes_t             FIROS3_sync(FIROS3Ctrl_t* unsafe psFIROS3Ctrl);
-#else
         FIROS3ReturnCodes_t             FIROS3_sync(FIROS3Ctrl_t* psFIROS3Ctrl);
-#endif
 
 /** This function pushes a single input sample into the filter
  *  It should be called three times for each FIROS3_proc call
@@ -113,11 +90,7 @@
  *  \param      *psFIROS3Ctrl   OS3 control structure of type FIROS3Ctrl_t
  *  \returns    Error code of type FIROS3ReturnCodes_t
  */
-#ifdef __XC__
-        FIROS3ReturnCodes_t             FIROS3_input(FIROS3Ctrl_t* unsafe psFIROS3Ctrl);
-#else
         FIROS3ReturnCodes_t             FIROS3_input(FIROS3Ctrl_t* psFIROS3Ctrl);
-#endif
 
 /** This function performs the oversampling by 3 and outputs one sample
  *  The input and output buffers are pointed to by members of the psFIROS3Ctrl structure
@@ -126,11 +99,12 @@
  *  \param      *psFIROS3Ctrl   OS3 control structure of type FIROS3Ctrl_t
  *  \returns    Error code of type FIROS3ReturnCodes_t
  */
-#ifdef __XC__
-        FIROS3ReturnCodes_t             FIROS3_proc(FIROS3Ctrl_t* unsafe psFIROS3Ctrl);    
-#else
         FIROS3ReturnCodes_t             FIROS3_proc(FIROS3Ctrl_t* psFIROS3Ctrl);    
-#endif
+
     #endif // nINCLUDE_FROM_ASM
+
+#ifdef __XC__
+}
+#endif
 
 #endif // _FIROS3_H_
