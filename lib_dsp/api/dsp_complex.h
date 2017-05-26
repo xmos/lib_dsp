c53337f3f1c1abcf916bf8cb1ce0deed15f7c323
Henk Muller <henk@xmos.com>
2017-05-26 14:11:13 +0100

    Added basic complex operations and complex FIR

--- lib_dsp/api/dsp_complex.h
+++ lib_dsp/api/dsp_complex.h
@@ -3,6 +3,8 @@
 #ifndef DSP_COMPLEX_H_
 #define DSP_COMPLEX_H_
 
+#include <stdint.h>
+
 typedef struct
 {
     int32_t re;
@@ -16,5 +18,42 @@ typedef struct
     int16_t im;
 }
 dsp_complex_short_t;
+
+/** Function that adds two complex numbers that use the same fixed point
+ * representation
+ *
+ * \param[in] a   first complex number
+ * \param[in] b   second complex number
+ *
+ * \returns       sum - it may overflow.
+ */
+dsp_complex_t dsp_complex_add(dsp_complex_t a, dsp_complex_t b);
+
+/** Function that multiplies two complex numbers. The fixed point
+ * representation of one number has to be specified, the result will use
+ * the fixed point representation of the other number.
+ *
+ * \param[in] a   first complex number
+ * \param[in] b   second complex number
+ * \param[in] N   Number of bits behind the binary point in one of the numbers
+ * 
+ * \returns       product - it may overflow.
+ */
+dsp_complex_t dsp_complex_mul(dsp_complex_t a, dsp_complex_t b, uint32_t N);
+
+/** Function that computes the inner product of two complex vectors. The
+ * representation of one vector has to be specified, the result will use
+ * the fixed point representation of the other number.
+ *
+ * \param[in] a   first complex vector
+ * \param[in] b   second complex vector
+ * \param[in] L   Length of the vectors
+ * \param[in] offset  starting point to use in vector a
+ * \param[in] N   Number of bits behind the binary point in one of the vectors
+ * 
+ * \returns       inner product - it may overflow.
+ */
+dsp_complex_t dsp_complex_fir(dsp_complex_t a[], dsp_complex_t b[],
+                              uint32_t L, uint32_t offset, uint32_t N);
 
 #endif
---
+++ lib_dsp/src/dsp_complex.xc
@@ -0,0 +1,27 @@
+#include <dsp_complex.h>
+
+dsp_complex_t dsp_complex_add(dsp_complex_t a, dsp_complex_t b) {
+    dsp_complex_t sum = { a.re + b.re, a.im + b.im };
+    return sum;
+}
+
+dsp_complex_t dsp_complex_mul(dsp_complex_t a, dsp_complex_t b, uint32_t N) {
+    int32_t re = (a.re * (int64_t) b.re - a.im * (int64_t) b.im) >> N;
+    int32_t im = (a.re * (int64_t) b.im + a.im * (int64_t) b.re) >> N;
+    dsp_complex_t sum = { re, im };
+    return sum;
+}
+
+#if !defined(__XS2A__)
+dsp_complex_t dsp_complex_fir(dsp_complex_t a[], dsp_complex_t b[],
+                              uint32_t L, uint32_t off, uint32_t N) {
+    int64_t re = 0;
+    int64_t im = 0;
+    for(unsigned i = 0; i < L; i++) {
+        re += a[i+off].re * (int64_t) b[i].re - a[i+off].im * (int64_t) b[i].im;
+        im += a[i+off].re * (int64_t) b[i].im + a[i+off].im * (int64_t) b[i].re;
+    }
+    dsp_complex_t sum = { re >> N, im >> N };
+    return sum;
+}
+#endif
