be60eacbdfac79efdb8ebb9c677abe2b455e55a5
Henk Muller <henk@xmos.com>
2017-07-11 18:19:43 +0100

    Added test environment for complex vector and bfp

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -3,10 +3,10 @@ import xmostest
 def runtest():
     resources = xmostest.request_resource("xsim")
      
-    tester = xmostest.ComparisonTester(open('bfp_test.expect'),
+    tester = xmostest.ComparisonTester(open('complex_test.expect'),
                                        'lib_dsp', 'simple_tests',
-                                       'app_bfp', {})
+                                       'app_complex', {})
      
     xmostest.run_on_simulator(resources['xsim'],
-                              '../AN00209_xCORE-200_DSP_Library/app_bfp/bin/app_bfp.xe',
+                              '../AN00209_xCORE-200_DSP_Library/app_complex/bin/app_complex.xe',
                               tester=tester, timeout=1200)
