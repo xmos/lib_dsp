f810734b7776b574d4e6b0f1aeedaf319876b48a
Henk Muller <henk@xmos.com>
2017-07-25 13:46:10 +0100

    Added functionality to do block floating point where real and imaginary parts are shifted by different amounts

--- lib_dsp/api/dsp_bfp.h
+++ lib_dsp/api/dsp_bfp.h
@@ -40,6 +40,26 @@ uint32_t dsp_bfp_cls(dsp_complex_t pts[], const uint32_t N);
  */
 void dsp_bfp_shl( dsp_complex_t pts[], const uint32_t N, const int32_t shift );
 
+/** This function shifts an array of complex numbers to adjust the headroom
+ * It shifts left if the shift argument is positive, or right if the shift
+ * argument is negative. Hence, a sequence::
+ *
+ *   unsigned ls = dsp_bfp_cls(x,N)
+ *   dsp_bfp_shl(x, ls-1, N)
+ *
+ * will result in one leading sign bit; minimising headroom and maximising
+ * dynamic range.
+ *
+ * Requires at most 6 x N instructions.
+ *
+ * \param[in,out] pts       Array of dsp_complex_t elements.
+ * \param[in]     N         Number of points. Must be a power of two.
+ * \param[in]     shift_re  Number of places to shift the real part left by.
+ * \param[in]     shift_im  Number of places to shift the imaginary part left by
+ */
+void dsp_bfp_shl2( dsp_complex_t pts[], const uint32_t N,
+                   const int32_t shift_re, const int32_t shift_im );
+
 /** This function shifts an array of complex numbers to adjust the headroom,
  * whilst simultaneously shuffling the array around in preparation for an FFT.
  * It shifts left if the shift argument is positive, or right if the shift
