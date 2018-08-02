925a70a310d7c50f96fe8e6e9038b78267ea63e4
Oscar Bailey <oscarbailey@xmos.com>
2018-08-02 12:41:52 +0100

    Update Copyright, source check now passing

--- lib_dsp/src/gen/generate_window.py
+++ lib_dsp/src/gen/generate_window.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, XMOS Ltd, All rights reserved
+# Copyright (c) 2015-2018, XMOS Ltd, All rights reserved
 import numpy
 from numpy import log10, abs, pi
 import scipy
--- lib_dsp/src/logistics_table.py
+++ lib_dsp/src/logistics_table.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, XMOS Ltd, All rights reserved
+# Copyright (c) 2017-2018, XMOS Ltd, All rights reserved
 import numpy as np
 import matplotlib.pyplot as plt
 
