ce48a5c366813fbbc3730862dc3251cfa73a9822
Daniel Pieczko <danielpieczko@xmos.com>
2021-03-17 08:49:50 +0000

    Update copyright notices and licence references

--- tests/test_biquad/src/iir.h
+++ tests/test_biquad/src/iir.h
@@ -1,5 +1,5 @@
-// Copyright (c) 2019-2021, XMOS Ltd, All rights reserved
-// This software is available under the terms provided in LICENSE.txt.
+// Copyright 2019-2021 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #include "dsp.h"
 #define IIR_ORDERS 8
 #define MAX_BIQUAD_ORDER 4
